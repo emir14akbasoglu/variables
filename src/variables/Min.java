21fed74329eed8156703f60b77ddf9a215da344d
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 16:12:10 +0300

    Initial commit

---
+++ src/variables/Min.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class Min {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName="Emir";
+		int studentAge=15;
+		System.out.println(studentName);
+		studentName="murat";
+		System.out.println(studentAge);
+		System.out.println(studentName+"'ýn yaþý "+studentAge);
+		String studentNationalIdentity="46876541231";
+		System.out.println("TC Kimlik Numarasý  :"
+		+studentNationalIdentity);
+		double studentLength=1.78;
+		System.out.println("ögrencinin Boyu:"+studentLength);
+		boolean studentIsActive=true;//ture yada false
+		System.out.println("ögrenci Devam :"+studentIsActive);
+	}
+		
+	
+
+	
+
+}
+
